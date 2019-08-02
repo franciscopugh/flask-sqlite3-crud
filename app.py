30d51f0969ef13240479047822eb833546cdad8a
TheDarkWolf <franciscopugh01@gmail.com>
2019-08-02 20:10:54 -0300

    Graphic Interface Created

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/task.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
 db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+@app.route('/add-task', methods=['POST'])
+def create():
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
