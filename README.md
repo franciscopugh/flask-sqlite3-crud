9b9b26aa3c9f11cc6dc3434d28f85ca783e8eb95
TheDarkWolf <franciscopugh01@gmail.com>
2019-08-03 01:23:14 -0300

    Last commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React-api-weather
+_First application created with react using a weather api_
+
+## Screenshot 📌
--- app.py
+++ app.py
@@ -22,5 +22,18 @@ def create():
     db.session.commit()
     return redirect(url_for('index'))
 
+@app.route('/done/<id>')
+def done(id):
+    task = Task.query.filter_by(id=int(id)).first()
+    task.done = not(task.done)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<id>')
+def delete(id):
+    task = Task.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
