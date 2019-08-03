f42a31ff664b9c7b01d18b9b7b84ecea7126f6c7
TheDarkWolf <franciscopugh01@gmail.com>
2019-08-03 00:41:03 -0300

    tasks can now be created

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
 db = SQLAlchemy(app)
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(300))
+    done = db.Column(db.Boolean)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks = tasks)
 
 @app.route('/add-task', methods=['POST'])
 def create():
-    
+    task = Task(content=request.form['content'], done=False)
+    db.session.add(task)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
